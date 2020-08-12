01ffd6167312fa349eeff1061956fadc4e1e32f8
usmanov647@gmail.com <vagrant@vagrant.vm>
2020-08-12 06:07:07 +0000

    added a POST handler for light color

--- server/usman/views.py
+++ server/usman/views.py
@@ -15,9 +15,14 @@ def control(request, zone_name):
     zone_selected = Zone.objects.get(zone_name=zone_name)
 
     if request.method == 'POST':
-        temp_set = int(request.POST['temp_set'])
-        if 15 <= temp_set <= 30:
-            zone_selected.temperature_set = temp_set
+        print(request.POST)
+        if 'temp_set' in request.POST:
+            temp_set = int(request.POST['temp_set'])
+            if 15 <= temp_set <= 30:
+                zone_selected.temperature_set = temp_set
+                zone_selected.save()
+        if 'color_set' in request.POST:
+            zone_selected.light_color = request.POST['color_set']
             zone_selected.save()
     context = {
         'zone_selected': zone_selected
